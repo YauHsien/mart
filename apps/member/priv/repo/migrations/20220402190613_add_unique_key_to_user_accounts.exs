ebcf9ddd9206f8174f243d169706f34e4b809c77
Yau-Hsien Huang <yauhsienhuang@gmail.com>
2022-04-03 13:03:20 +0800

    Modify unique constraint to user_accounts.username to case-insensitive one

--- apps/member/priv/repo/migrations/20220402190613_add_unique_key_to_user_accounts.exs
+++ apps/member/priv/repo/migrations/20220402190613_add_unique_key_to_user_accounts.exs
@@ -2,6 +2,6 @@ defmodule M.Member.Repo.Migrations.AddUniqueKeyToUserAccounts do
   use Ecto.Migration
 
   def change do
-    create unique_index(:user_accounts, :username, name: :uk_username)
+    create unique_index(:user_accounts, ["(lower(username))"], name: :uk_username)
   end
 end
