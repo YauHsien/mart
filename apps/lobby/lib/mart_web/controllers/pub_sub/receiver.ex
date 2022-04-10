7334883cbb2608c05f5447fef0fd9ed0d1241ba3
Yau-Hsien Huang <yauhsienhuang@gmail.com>
2022-04-10 23:54:55 +0800

    Add a simple gen-server with name registration

---
+++ apps/lobby/lib/mart_web/controllers/pub_sub/receiver.ex
@@ -0,0 +1,20 @@
+defmodule M.LobbyWeb.Controllers.PubSub.Receiver do
+	use GenServer, id: __MODULE__
+
+
+  def start_link(args),
+    do: GenServer.start_link(__MODULE__, args, name: __MODULE__)
+
+
+
+  def init(args),
+    do: {:ok, %{args: args}}
+
+
+
+  def handle_call(:hello, _from, state),
+    do: {:reply, state, state}
+
+
+
+end
