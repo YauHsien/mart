b5ca0b3513d70f5d166b6cde7d7c55f90ff2f3f3
Yau-Hsien Huang <yauhsienhuang@gmail.com>
2022-04-13 02:21:45 +0800

    Arrange shop actions

--- apps/shop/lib/shop.ex
+++ apps/shop/lib/shop.ex
@@ -3,6 +3,10 @@ defmodule M.Shop do
   Documentation for `M.Shop`.
   """
   use GenServer
+  require M.Shop.Resource.Action
+  alias M.Shop.Resource.Action
+  alias Phoenix.PubSub
+
 
 
 
@@ -18,12 +22,20 @@ defmodule M.Shop do
 
 
 
-  @spec init([{:name, String.t()} | {:channel, Phoenix.PubSub.t()}]) :: {:ok, map()} | {:stop, reason :: term()}
+  @spec init([{:name, GenServer.server()} | {:channel, PubSub.t()}]) :: {:ok, map()} | {:stop, reason :: term()}
 
   @impl true
-  def init(args),
-    do: {:ok, %{name: Keyword.get(args,:name),
-                channel: Keyword.get(args,:channel)}}
+  def init(args) do
+    channel = Keyword.get(args, :channel)
+    Action.serve(channel, [
+          Action.action_shop_listings(),
+          Action.action_shop_item(),
+          Action.action_shop_item_detail()
+        ])
+    {:ok, %{ name: Keyword.get(args, :name),
+             channel: channel
+           }}
+  end
 
 
 
@@ -32,5 +44,47 @@ defmodule M.Shop do
   def handle_call(:hello, _from, state) do
 	  {:reply, state, state}
   end
+
+
+
+
+  @impl true
+
+  @spec handle_info(msg :: Action.t() | :timeout | term(), state :: term()) ::
+  {:noreply, new_state}
+  | {:noreply, new_state, timeout() | :hibernate | {:continue, term()}}
+  | {:stop, reason :: term(), new_state}
+  when new_state: term()
+
+  def handle_info(msg, state)
+
+
+  def handle_info(
+    %{
+      action_type: action_type,
+      action_id: action_id,
+      return_addr: return_addr,
+      payload: payload
+    },
+    %{
+      name: my_name,
+      channel: pub_sub
+    } = state) do
+
+    solution = Action.solve(my_name, action_type, payload)
+    result = Map.merge(%{
+          return_addr: return_addr,
+          action_type: action_type,
+          action_id: action_id
+                       }, solution)
+    PubSub.broadcast!(pub_sub, return_addr, result)
+
+    {:noreply, state}
+  end
+
+
+  def handle_info(_msg, state),
+    do: {:noreply, state}
+
 
 end
--- apps/shop/lib/shop/resource/action.ex
+++ apps/shop/lib/shop/resource/action.ex
@@ -1,8 +1,95 @@
 defmodule M.Shop.Resource.Action do
+  alias M.Shop.Resource.Action
+  alias Phoenix.PubSub
 
-  @type t() :: {:action, atom()}
+  @type type() :: {:action, atom()}
+
+  @type t() :: %{
+    action_type: Action.type(),
+    action_id: any(),
+    return_addr: PubSub.topic(),
+    payload: map()
+  }
+
+  @type result() :: %{
+    status: :ok | :error,
+    payload: any()
+  }
 
 	defmacro action_shop_listings, do: {:action, :'shop:listings'}
+  defmacro reqpar_shop_listings(shop_id),
+    do: %{
+          shop_id: shop_id
+    }
+
   defmacro action_shop_item, do: {:action, :'shop:item'}
-  defmacro action_shop_item_details, do: {:action, :'shop:item:details'}
+  defmacro reqpar_shop_item(shop_id, item_id),
+    do: %{
+          shop_id: shop_id,
+          item_id: item_id
+    }
+
+  defmacro action_shop_item_detail, do: {:action, :'shop:item:details'}
+  defmacro reqpar_shop_item_detail(shop_id, item_id, detail_id),
+    do: %{
+          shop_id: shop_id,
+          item_id: item_id,
+          detail_id: detail_id
+    }
+
+
+
+
+  @spec serve(pub_sub :: PubSub.t(), [action_type :: Action.type()]) :: :ok | {:error, term()}
+
+  def serve(pub_sub, action_type_list),
+    do: for action_type <- action_type_list, do: PubSub.subscribe(pub_sub, inspect(action_type))
+
+
+
+
+  @spec request(action_type :: Action.t(), action_id :: any(), return_addr :: any(), pub_sub :: PubSub.t(), payload :: map()) :: :ok
+
+  def request(action_type, action_id, return_addr, pub_sub, payload) do
+    PubSub.broadcast!(pub_sub,
+      inspect(action_type),
+      %{
+        action_type: action_type,
+        action_id: action_id,
+        return_addr: return_addr,
+        payload: payload
+      })
+  end
+
+
+
+
+  @spec solve(shop :: GenServer.server(), action_type :: Action.t(), payload :: map()) :: result when result: result()
+
+  def solve(shop, action_type, content)
+
+
+  def solve(_shop, action_shop_listings() = _action_type, %{shop_id: _shop_id} = _content) do
+    status: :do_shop_listings # TODO 處理店家資訊的查詢，希望分為二端：如果是 shop gen-server 快取的，就調用 shop gen-server ；否則可往 Repo 方查詢。
+    %{status: status, payload: payload}
+  end
+
+
+  def solve(_shop, action_shop_item() = _action_type, %{shop_id: shop_id, item_id: item_id} = _content) do
+    status: :do_shop_item # TODO
+    %{status: status, payload: payload}
+  end
+
+
+  def solve(_shop, action_shop_item_detail() = _action_type, %{shop_id: shop_id, item_id: item_id, detail_id: detail_id} = _content) do
+    status: :do_shop_item_detail # TODO
+    %{status: status, payload: payload}
+  end
+
+
+  def shop(_shop, _action_type, _content),
+    do: %{status: :error}
+
+
+
 end
