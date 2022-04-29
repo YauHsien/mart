51ae93231ed51689760d31d996562cf0183267c2
Yau-Hsien Huang <yauhsienhuang@gmail.com>
2022-04-29 17:50:33 +0800

    Give channel-messages note.

--- apps/core/lib/core/common/repo_message.ex
+++ apps/core/lib/core/common/repo_message.ex
@@ -1,4 +1,7 @@
 defmodule M.Core.Common.RepoMessage do
+  @moduledoc """
+  Terms to use in pub-sub channel of repositories tier.
+  """
 
   @type id() :: integer() | String.t()
   @type table_name() :: atom()
@@ -12,11 +15,11 @@ defmodule M.Core.Common.RepoMessage do
   @type return_addr() :: String.t()
 
 
-  @aggregate2 "{:aggregate, aggregate}"
-  @aggregate3 "{:aggregate, aggregate, id: id}"
-  @field4 "{:field, field, id: id, field: field_name, value: value}"
-  @list1 "{:list, thing}"
-  @object2 "{:object, object, id: id}"
+  @aggregate2 "{:aggregate,` _aggregate_ `}"
+  @aggregate3 "{:aggregate,` _aggregate_ `, id:` _id_ `}"
+  @field4 "{:field,` _field_ `, id:` _id_ `, field:` _field\_name_ `, value:` _value_ `}"
+  @list1 "{:list,` _thing_ `}"
+  @object2 "{:object,` _object_ `, id:` _id_ `}"
   @collection [
     @aggregate2,
     @aggregate3,
@@ -26,23 +29,23 @@ defmodule M.Core.Common.RepoMessage do
   ]
 
 
-  @doc "Message `#{@aggregate2}``."
+  @doc "Message pattern `#{@aggregate2}`."
   defmacro aggregate(aggregate),
     do: quote do: {:aggregate, unquote(aggregate)}
 
-  @doc "Message `#{@aggregate3}`."
+  @doc "Message pattern `#{@aggregate3}`."
   defmacro aggregate(aggregate, id),
     do: quote do: {:aggregate, unquote(aggregate), id: unquote(id)}
 
-  @doc "Message `#{@field4}`."
+  @doc "Message pattern `#{@field4}`."
   defmacro field(field, id, field_name, value),
     do: quote do: {:field, unquote(field), id: unquote(id), field: unquote(field_name), value: unquote(value)}
 
-  @doc "Message `#{@list1}` as list of things."
+  @doc "Message pattern `#{@list1}` as list of things."
   defmacro list(thing),
     do: quote do: {:list, unquote(thing)}
 
-  @doc "Message `#{@object2}`."
+  @doc "Message pattern `#{@object2}`."
   defmacro object(object, id),
     do: quote do: {:object, unquote(object), id: unquote(id)}
 
@@ -51,7 +54,7 @@ defmodule M.Core.Common.RepoMessage do
   @doc @collection
   |> Enum.map(&( "`\"topic #{&1}\"`" ))
   |> Enum.join(", ")
-  |> then(&( "String such as\n" <> &1 ))
+  |> then(&( "Topic to use in a pub-sub channel. String such as\n" <> &1 ))
 
   @spec topic(t()) :: topic()
   def topic(name),
@@ -61,7 +64,11 @@ defmodule M.Core.Common.RepoMessage do
   @doc @collection
   |> Enum.map(&( "`\"return topic #{&1}\"`" ))
   |> Enum.join(", ")
-  |> then(&( "String such as\n" <> &1 ))
+  |> then(&( """
+  Topic as return address to use in a pub-sub channel. That means to share messages channel-wisely.
+
+  String such as #{&1}.
+  """ ))
 
   @spec return(topic()) :: return_addr()
   def return(topic),
@@ -69,9 +76,13 @@ defmodule M.Core.Common.RepoMessage do
 
 
   @doc @collection
-  |> Enum.map(&( "`\"return topic #{&1}\" to _id_`" ))
+  |> Enum.map(&( "`\"return topic #{&1} to ` _id_ `\"`" ))
   |> Enum.join(", ")
-  |> then(&( "String such as\n" <> &1 ))
+  |> then(&( """
+  Topic as return address to use in a pub-sub channel. That means to share messages id-wisely.
+
+  String such as #{&1}.
+  """ ))
 
   @spec return(topic(), id()) :: return_addr()
   def return(topic, id),
