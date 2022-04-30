641add9828d7f5b0b62e6d2ad7e719dd15bb386f
Yau-Hsien Huang <yauhsienhuang@gmail.com>
2022-05-01 04:43:50 +0800

    Complete domain Member

--- apps/member/lib/domain/user_account.ex
+++ apps/member/lib/domain/user_account.ex
@@ -29,6 +29,36 @@ defmodule M.Domain.Member.UserAccount do
 
   def create(username, password, datetime \\ NaiveDateTime.utc_now()) do
 
+    pac_name = M.Domain.Member.username(username)
+    pac_pass = create_password(username, password, datetime)
+    pac_token = UserToken.create(pac_name, pac_pass)
+
+    %__MODULE__{
+      name: pac_name,
+      password: pac_pass,
+      user_token: pac_token,
+      token_history: []
+    }
+  end
+
+
+
+  @spec set_password(t(), String.t()) :: t()
+
+  @spec set_password(t(), String.t(), NaiveDateTime.t()) :: t()
+
+  def set_password(user_account, password, datetime \\ NaiveDateTime.utc_now()) do
+
+    M.Domain.Member.username(username) = user_account.name
+    pac_pass = create_password(username, password, datetime)
+
+    %__MODULE__{ user_account | password: pac_pass }
+  end
+
+
+
+  defp create_password(username, password, datetime) do
+
     salt =
       datetime
       |> NaiveDateTime.to_gregorian_seconds()
@@ -36,21 +66,77 @@ defmodule M.Domain.Member.UserAccount do
       |> Integer.to_string(16)
     enc_pass = Crypto.sign(password, salt, username)
 
-    pac_user = M.Domain.Member.username(username)
-    pac_pass = M.Domain.Member.password(enc_pass, salt, datetime)
-    pac_token = UserToken.create(
-      pac_user,
-      pac_pass
-    )
+    M.Domain.Member.password(enc_pass, salt, datetime)
+  end
 
-    %M.Domain.Member.UserAccount{
-      name: pac_user,
-      password: pac_pass,
-      user_token: pac_token,
-      token_history: []
+
+
+  @spec verify_password(t(), String.t()) :: :ok | :invalid
+
+  def verify_password(user_account, plain_password) do
+
+    M.Domain.Member.username(username) = user_account.name
+    M.Domain.Member.password(enc_pass, salt, _) = user_account.password
+
+    case Crypto.verify(plain_password, salt, enc_pass) do
+      term when term == {:ok, username} or term == {:error, :expired} ->
+        :ok
+      {:error, :invalid} ->
+        :invalid
+    end
+  end
+
+
+
+  @spec new_token(t(), NaiveDateTime.t()) :: t()
+
+  def new_token(user_account, expired_when \\ nil) do
+
+    %__MODULE__{
+      user_account |
+      user_token: UserToken.create(user_account.name, user_account.password, expired_when),
+      token_history: [ user_account.user_token | user_account.token_history ]
     }
   end
 
+
+
+  @spec renew_token(t(), String.t()) :: t() | :invalid
+
+  def renew_token(user_account, plain_token, expired_when \\ nil) do
+
+    M.Domain.Member.username(username) = user_account.name
+    M.Domain.Member.password(enc_pass, salt, _) = user_account.password
+
+    case Crypto.verify(plain_token, salt, enc_pass) do
+
+      term when term == {:ok, username} or term == {:error, :expired} ->
+        new_token(user_account, expired_when)
+
+      {:error, :invalid} ->
+        :invalid
+
+    end
+  end
+
+
+
+  @spec verify_token(t(), String.t()) :: :ok | :expired | :invalid
+
+  def verify_token(user_account, plain_token) do
+
+    M.Domain.Member.username(username) = user_account.name
+    M.Domain.Member.password(enc_pass, salt, _) = user_account.password
+
+    case Crypto.verify(plain_token, salt, enc_pass) do
+      {:ok, ^username} ->
+        :ok
+      {:error, :expired} ->
+        :expired
+      {:error, :invalid} ->
+        :invalid
+    end
+  end
 
 
 end
