ad5e3f6c5783b746fa8819816cf2326a1eb3a84b
Yau-Hsien Huang <yauhsienhuang@gmail.com>
2022-05-26 17:11:26 +0800

    Add dependencies for :data_cache

--- apps/data_cache/mix.exs
+++ apps/data_cache/mix.exs
@@ -21,8 +21,10 @@ defmodule M.DataCache.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:ex_domain_toolkit, "~> 0.1.0", github: "YauHsien/ex_domain_toolkit", branch: "main"},
+      {:typed_struct, "~> 0.1.4"},
+      {:phoenix_pubsub, "~> 2.1.1"},
+      {:mart_core, "~> 0.1.0", app: false, github: "YauHsien/mart", branch: "main", sparse: "apps/core"}
     ]
   end
 end
